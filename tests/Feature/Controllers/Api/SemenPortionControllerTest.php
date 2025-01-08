824c2cac159ebe62bdf499f515709a6608a7e1df
averines <averines@mail.ru>
2025-01-08 21:07:54 +0300

    tests: semen-portion

---
+++ tests/Feature/Controllers/Api/SemenPortionControllerTest.php
@@ -0,0 +1,285 @@
+<?php
+
+namespace Feature\Controllers\Api;
+
+use AllowDynamicProperties;
+use App\Models\SemenPortion;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+#[AllowDynamicProperties] class SemenPortionControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('db:seed');
+    }
+
+    public function test_index_for_admin()
+    {
+        $response = $this->actingAs($this->admin)->getJson(route('api.semen-portions.index'));
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'items',
+                "current_page",
+                'first_page_url',
+                'from',
+                'last_page',
+                'last_page_url',
+                'next_page_url',
+                'path',
+                'per_page',
+                'prev_page_url',
+                'to',
+                'total',
+            ]
+        ]);
+    }
+
+    public function test_index_for_non_admin()
+    {
+        $response = $this->actingAs($this->user)->getJson(route('api.semen-portions.index'));
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'items',
+                "current_page",
+                'first_page_url',
+                'from',
+                'last_page',
+                'last_page_url',
+                'next_page_url',
+                'path',
+                'per_page',
+                'prev_page_url',
+                'to',
+                'total',
+            ]
+        ]);
+    }
+
+    public function test_store_for_admin()
+    {
+        $data = [
+            'name' => 'Test Name',
+            'description' => 'Test Description',
+            'is_active' => true,
+        ];
+
+        $response = $this->actingAs($this->admin)->postJson(route('api.semen-portions.store'), $data);
+        $response->assertSuccessful();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'id',
+                'name',
+                'description',
+                'is_active',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+
+        $this->assertDatabaseHas('semen_portions', $data);
+    }
+
+    public function test_store_for_non_admin()
+    {
+        $data = [
+            'name' => 'Test Name',
+            'description' => 'Test Description',
+            'is_active' => true,
+        ];
+
+        $response = $this->actingAs($this->user)->postJson(route('api.semen-portions.store'), $data);
+        $response->assertSuccessful();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'id',
+                'name',
+                'description',
+                'is_active',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+
+        $this->assertDatabaseHas('semen_portions', $data);
+    }
+
+    public function test_show_for_admin()
+    {
+        $item = SemenPortion::query()->first();
+        $response = $this->actingAs($this->admin)->json('GET', route('api.semen-portions.show', $item));
+
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'semenPortion' => [
+                    'id',
+                    'name',
+                    'description',
+                    'is_active',
+                    'created_at',
+                    'updated_at',
+                ]
+            ],
+        ]);
+        $response->assertJson([
+            'data' => [
+                'semenPortion' => [
+                    'id' => $item->id,
+                    'name' => $item->name,
+                    'description' => $item->description,
+                    'is_active' => $item->is_active,
+                    'created_at' => $item->created_at,
+                    'updated_at' => $item->updated_at,
+                ]
+            ]
+        ]);
+    }
+
+    public function test_show_for_non_admin()
+    {
+        $item = SemenPortion::query()->first();
+        $response = $this->actingAs($this->admin)->json('GET', route('api.semen-portions.show', $item));
+
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'semenPortion' => [
+                    'id',
+                    'name',
+                    'description',
+                    'is_active',
+                    'created_at',
+                    'updated_at',
+                ]
+            ],
+        ]);
+        $response->assertJson([
+            'data' => [
+                'semenPortion' => [
+                    'id' => $item->id,
+                    'name' => $item->name,
+                    'description' => $item->description,
+                    'is_active' => $item->is_active,
+                    'created_at' => $item->created_at,
+                    'updated_at' => $item->updated_at,
+                ]
+            ]
+        ]);
+    }
+
+    public function test_update_for_admin()
+    {
+        $item = SemenPortion::query()->first();
+
+        $data = [
+            'name' => 'Updated Name',
+            'description' => 'Updated Description',
+            'is_active' => false,
+        ];
+
+        $response = $this->actingAs($this->admin)->putJson(route('api.semen-portions.update', $item->id), $data);
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'semenPortion' => [
+                    'id',
+                    'name',
+                    'description',
+                    'is_active',
+                    'created_at',
+                    'updated_at',
+                ]
+            ],
+        ]);
+
+        $this->assertDatabaseHas('semen_portions', $data);
+    }
+
+    public function test_update_for_non_admin()
+    {
+        $item = SemenPortion::query()->first();
+
+        $data = [
+            'name' => 'Updated Name',
+            'description' => 'Updated Description',
+            'is_active' => false,
+        ];
+
+        $response = $this->actingAs($this->user)->putJson(route('api.semen-portions.update', $item->id), $data);
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'semenPortion' => [
+                    'id',
+                    'name',
+                    'description',
+                    'is_active',
+                    'created_at',
+                    'updated_at',
+                ]
+            ],
+        ]);
+
+        $this->assertDatabaseHas('semen_portions', $data);
+    }
+
+    public function test_destroy_for_admin()
+    {
+        $item = SemenPortion::query()->first();
+
+        $response = $this->actingAs($this->admin)->deleteJson(route('api.semen-portions.destroy', $item->id));
+
+        $response->assertOk();
+        $response->assertJson([
+            'success' => true,
+            'error' => null,
+        ]);
+
+        $this->assertDatabaseMissing('semen_portions', ['id' => $item->id]);
+    }
+
+    public function test_destroy_for_non_admin()
+    {
+        $item = SemenPortion::query()->first();
+
+        $response = $this->actingAs($this->user)->deleteJson(route('api.semen-portions.destroy', $item->id));
+
+        $response->assertOk();
+        $response->assertJson([
+            'success' => true,
+            'error' => null,
+        ]);
+
+        $this->assertDatabaseMissing('semen_portions', ['id' => $item->id]);
+    }
+}
