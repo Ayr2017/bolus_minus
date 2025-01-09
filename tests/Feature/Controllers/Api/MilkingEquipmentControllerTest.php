ee0f73c0bd6d494d5106d784f3d7445850b6d60c
averines <averines@mail.ru>
2025-01-09 23:37:38 +0300

    tests: milking-equipment

---
+++ tests/Feature/Controllers/Api/MilkingEquipmentControllerTest.php
@@ -0,0 +1,409 @@
+<?php
+
+namespace Feature\Controllers\Api;
+
+use AllowDynamicProperties;
+use App\Models\MilkingEquipment;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Enums\EquipmentType;
+use App\Models\Organisation;
+
+#[AllowDynamicProperties] class MilkingEquipmentControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('db:seed');
+        $this->artisan('db:seed --class=OrganisationSeeder');
+        MilkingEquipment::factory()->count(1)->create();
+    }
+
+    public function test_index_for_admin()
+    {
+        $response = $this->actingAs($this->admin)->getJson(route('api.milking-equipments.index'));
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'items',
+                "current_page",
+                'first_page_url',
+                'from',
+                'last_page',
+                'last_page_url',
+                'next_page_url',
+                'path',
+                'per_page',
+                'prev_page_url',
+                'to',
+                'total',
+            ]
+        ]);
+    }
+
+    public function test_index_for_non_admin()
+    {
+        $response = $this->actingAs($this->user)->getJson(route('api.milking-equipments.index'));
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'items',
+                "current_page",
+                'first_page_url',
+                'from',
+                'last_page',
+                'last_page_url',
+                'next_page_url',
+                'path',
+                'per_page',
+                'prev_page_url',
+                'to',
+                'total',
+            ]
+        ]);
+    }
+
+    public function test_store_for_admin()
+    {
+        $data = [
+            'organization_id' => 1,
+            'department_id' => 1,
+            'equipment_type' => EquipmentType::PARALLEL->value,
+            'milking_places_amount' => 1,
+            'milking_per_day_amount' => 1, //TODO: уточнить. здесь поле обязательно, а в бд не обязательно -> сделать обязательным?
+        ];
+
+        $response = $this->actingAs($this->admin)->postJson(route('api.milking-equipments.store'), $data);
+        $response->assertSuccessful();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'id',
+                'equipment_type', // TODO: пофиксить, возвращает null
+                'created_at',
+                'milking_places_amount', // TODO: уточнить - не возвращает поле - это ок?
+                'milking_per_day_amount', // TODO: уточнить - не возвращает поле - это ок?
+                'organization' => [
+                    'id',
+                    'uuid',
+                    'name',
+                    'structural_unit_id',
+                    'parent_id',
+                    'is_active',
+                    'created_at',
+                    'updated_at',
+                ],
+                'department' => [
+                    'id',
+                    'uuid',
+                    'name',
+                    'structural_unit_id',
+                    'parent_id',
+                    'is_active',
+                    'created_at',
+                    'updated_at',
+                ],
+            ],
+        ]);
+
+        $this->assertDatabaseHas('milking_equipments', $data);
+    }
+
+    public function test_store_for_non_admin()
+    {
+        $data = [
+            'organization_id' => 1,
+            'department_id' => 1,
+            'equipment_type' => EquipmentType::PARALLEL->value,
+            'milking_places_amount' => 1,
+            'milking_per_day_amount' => 1, //TODO: уточнить. здесь поле обязательно, а в бд не обязательно -> сделать обязательным?
+        ];
+
+        $response = $this->actingAs($this->user)->postJson(route('api.milking-equipments.store'), $data);
+        $response->assertSuccessful();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                'id',
+                'equipment_type', // TODO: пофиксить, возвращает null
+                'created_at',
+                'milking_places_amount', // TODO: уточнить - не возвращает поле - это ок?
+                'milking_per_day_amount', // TODO: уточнить - не возвращает поле - это ок?
+                'organization' => [
+                    'id',
+                    'uuid',
+                    'name',
+                    'structural_unit_id',
+                    'parent_id',
+                    'is_active',
+                    'created_at',
+                    'updated_at',
+                ],
+                'department' => [
+                    'id',
+                    'uuid',
+                    'name',
+                    'structural_unit_id',
+                    'parent_id',
+                    'is_active',
+                    'created_at',
+                    'updated_at',
+                ],
+            ],
+        ]);
+
+        $this->assertDatabaseHas('milking_equipments', $data);
+    }
+
+    public function test_show_for_admin()
+    {
+        $item = MilkingEquipment::query()->first();
+
+        $response = $this->actingAs($this->admin)->json('GET', route('api.milking-equipments.show', $item));
+
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                [
+                    'id',
+                    'equipment_type',
+                    'created_at',
+                    'milking_places_amount', // TODO: уточнить - не возвращает поле - это ок?
+                    'milking_per_day_amount', // TODO: уточнить - не возвращает поле - это ок?
+                    'organization' => [
+                        'id',
+                        'uuid',
+                        'name',
+                        'structural_unit_id',
+                        'parent_id',
+                        'is_active',
+                        'created_at',
+                        'updated_at',
+                    ],
+                    'department' => [
+                        'id',
+                        'uuid',
+                        'name',
+                        'structural_unit_id',
+                        'parent_id',
+                        'is_active',
+                        'created_at',
+                        'updated_at',
+                    ],
+                ]
+            ],
+        ]);
+        $response->assertJson([
+            'data' => [
+                [
+                    'id' => $item->id,
+                    'equipment_type' => $item->equipment_type, // TODO: пофиксить, возвращает null
+                    'created_at' => $item->created_at->toDateTimeString(),
+                    'organization' => $item->organization->toArray(),
+                    'department' => $item->department->toArray(),
+                ]
+            ],
+        ]);
+    }
+
+    public function test_show_for_non_admin()
+    {
+        $item = MilkingEquipment::query()->first();
+
+        $response = $this->actingAs($this->user)->json('GET', route('api.milking-equipments.show', $item));
+
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                [
+                    'id',
+                    'equipment_type',
+                    'created_at',
+                    'milking_places_amount', // TODO: уточнить - не возвращает поле - это ок?
+                    'milking_per_day_amount', // TODO: уточнить - не возвращает поле - это ок?
+                    'organization' => [
+                        'id',
+                        'uuid',
+                        'name',
+                        'structural_unit_id',
+                        'parent_id',
+                        'is_active',
+                        'created_at',
+                        'updated_at',
+                    ],
+                    'department' => [
+                        'id',
+                        'uuid',
+                        'name',
+                        'structural_unit_id',
+                        'parent_id',
+                        'is_active',
+                        'created_at',
+                        'updated_at',
+                    ],
+                ]
+            ],
+        ]);
+        $response->assertJson([
+            'data' => [
+                [
+                    'id' => $item->id,
+                    'equipment_type' => $item->equipment_type, // TODO: пофиксить, возвращает null
+                    'created_at' => $item->created_at->toDateTimeString(),
+                    'organization' => $item->organization->toArray(),
+                    'department' => $item->department->toArray(),
+                ]
+            ],
+        ]);
+    }
+
+    public function test_update_for_admin()
+    {
+        $data = [
+            'organization_id' => 1,
+            'department_id' => 1,
+            'equipment_type' => EquipmentType::PARALLEL->value,
+            'milking_places_amount' => 1,
+            'milking_per_day_amount' => 1,
+        ];
+
+        $item = MilkingEquipment::create($data);
+
+        $updatedData = [
+            'organization_id' => 2,
+            'department_id' => 2,
+            'equipment_type' => EquipmentType::HERRINGBONE->value,
+            'milking_places_amount' => 2,
+            'milking_per_day_amount' => 2,
+        ];
+
+        $response = $this->actingAs($this->admin)->putJson(route('api.milking-equipments.update', $item), $updatedData);
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                [
+                    'id',
+                    'equipment_type',
+                    'created_at',
+                    'organization',
+                    'department',
+                ]
+            ],
+        ]);
+        $response->assertJson([
+            'data' => [
+                [
+                    'id' => $item->id,
+                    'equipment_type' => $updatedData['equipment_type'], // TODO: пофиксить, возвращает null
+                    'created_at' => $item->created_at->toDateTimeString(),
+                    'organization' => Organisation::find($updatedData['organization_id'])->toArray(),
+                    'department' => Organisation::find($updatedData['department_id'])->toArray(),
+                ]
+            ],
+        ]);
+
+        $this->assertDatabaseHas('milking_equipments', $updatedData);
+    }
+
+    public function test_update_for_non_admin()
+    {
+        $data = [
+            'organization_id' => 1,
+            'department_id' => 1,
+            'equipment_type' => EquipmentType::PARALLEL->value,
+            'milking_places_amount' => 1,
+            'milking_per_day_amount' => 1,
+        ];
+
+        $item = MilkingEquipment::create($data);
+
+        $updatedData = [
+            'organization_id' => 2,
+            'department_id' => 2,
+            'equipment_type' => EquipmentType::HERRINGBONE->value,
+            'milking_places_amount' => 2,
+            'milking_per_day_amount' => 2,
+        ];
+
+        $response = $this->actingAs($this->user)->putJson(route('api.milking-equipments.update', $item), $updatedData);
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'message',
+            'success',
+            'error',
+            'data' => [
+                [
+                    'id',
+                    'equipment_type',
+                    'created_at',
+                    'organization',
+                    'department',
+                ]
+            ],
+        ]);
+        $response->assertJson([
+            'data' => [
+                [
+                    'id' => $item->id,
+                    'equipment_type' => $updatedData['equipment_type'], // TODO: пофиксить, возвращает null
+                    'created_at' => $item->created_at->toDateTimeString(),
+                    'organization' => Organisation::find($updatedData['organization_id'])->toArray(),
+                    'department' => Organisation::find($updatedData['department_id'])->toArray(),
+                ]
+            ],
+        ]);
+
+        $this->assertDatabaseHas('milking_equipments', $updatedData);
+    }
+
+    public function test_destroy_for_admin()
+    {
+        $item = MilkingEquipment::query()->first();
+
+        $response = $this->actingAs($this->admin)->deleteJson(route('api.milking-equipments.destroy', $item));
+
+        $response->assertOk();
+        $response->assertJson([
+            'success' => true,
+            'error' => null,
+        ]);
+
+        $this->assertDatabaseMissing('milking_equipments', ['id' => $item->id]);
+    }
+
+    public function test_destroy_for_non_admin()
+    {
+        $item = MilkingEquipment::query()->first();
+
+        $response = $this->actingAs($this->user)->deleteJson(route('api.milking-equipments.destroy', $item->id));
+
+        $response->assertOk();
+        $response->assertJson([
+            'success' => true,
+            'error' => null,
+        ]);
+
+        $this->assertDatabaseMissing('milking_equipments', ['id' => $item->id]);
+    }
+}
